23be87235143b5b10c4ce9c05ae07294ccd50ef9
yaochuang <chuang.yao1081@seaboxdata.com>
2025-09-30 17:04:46 +0800

    修正返回

--- src/main/java/com/github/wohatel/decoder/RpcMsgEncoder.java
+++ src/main/java/com/github/wohatel/decoder/RpcMsgEncoder.java
@@ -1,8 +1,6 @@
 package com.github.wohatel.decoder;
 
 import com.alibaba.fastjson2.JSON;
-import com.github.wohatel.constant.RpcErrorEnum;
-import com.github.wohatel.constant.RpcException;
 import com.github.wohatel.interaction.base.RpcMsg;
 import com.github.wohatel.interaction.base.RpcSession;
 import com.github.wohatel.interaction.common.ByteBufPoolManager;
@@ -19,8 +17,7 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
 
-/**
- * @author yaochuang
+/** * @author yaochuang
  */
 @Slf4j
 public class RpcMsgEncoder extends MessageToMessageEncoder<RpcMsg> {
@@ -30,41 +27,29 @@ public class RpcMsgEncoder extends MessageToMessageEncoder<RpcMsg> {
 
     @Override
     protected void encode(ChannelHandlerContext ctx, RpcMsg msg, List<Object> out) throws Exception {
-        CompositeByteBuf outerBuffer = ctx.alloc().compositeBuffer();
-        try {
-            ByteBuf encodeBuf = encodeMsg(ctx, msg);
-            outerBuffer.writeBoolean(msg.isNeedCompress());
-            if (msg.isNeedCompress()) {
-                ByteBuf compressBuf = tryCompress(encodeBuf);
-                outerBuffer.addComponent(true, compressBuf);
-            } else {
-                outerBuffer.addComponent(true, encodeBuf);
-            }
-            out.add(outerBuffer);
-        } catch (Throwable e) {
-            ReferenceByteBufUtil.safeRelease(outerBuffer);
-            log.error("rpcMsgEncoder encode error:", e);
-            throw e;
-        }
+        // Step1: 构建原始 ByteBuf（未压缩）
+        ByteBuf buffer = encodeMsg(ctx, msg);
+        ByteBuf target = tryCompress(buffer, msg.isNeedCompress());
+        out.add(target);
     }
 
     private ByteBuf encodeMsg(ChannelHandlerContext ctx, RpcMsg msg) {
-        CompositeByteBuf buffer = ctx.alloc().compositeBuffer();
-        try {
+        ByteBuf buffer = ctx.alloc().buffer();
+        return ReferenceByteBufUtil.exceptionRelease(() -> {
             buffer.writeBoolean(msg.isNeedCompress()); // 1: 是否压缩
             buffer.writeInt(msg.getRpcCommandType().getCode()); // 2: 消息体类型
+
             byte[] payloadBytes = JSON.toJSONBytes(msg.getPayload());
             buffer.writeInt(payloadBytes.length); // 3: 消息体长度
             buffer.writeBytes(payloadBytes);
+
             ByteBuf fileBuffer = msg.getByteBuffer();
-            if (fileBuffer == null) {
-                buffer.writeInt(0);
-            } else {
-                if (fileBuffer.refCnt() <= 0) {
-                    throw new RpcException(RpcErrorEnum.SEND_MSG, "fileBuffer is released");
-                }
-                buffer.addComponent(true, fileBuffer);
+            int fileLength = fileBuffer == null ? 0 : fileBuffer.readableBytes();
+            buffer.writeInt(fileLength); // 4: 文件长度
+            if (fileLength > 0) {
+                buffer.writeBytes(fileBuffer);
             }
+
             if (msg.getRpcCommandType() == RpcCommandType.file) {
                 RpcFileRequest rpcFileRequest = msg.getPayload(RpcFileRequest.class);
                 RpcSession rpcSession = rpcFileRequest.getRpcSession();
@@ -74,30 +59,28 @@ public class RpcMsgEncoder extends MessageToMessageEncoder<RpcMsg> {
                 }
             }
             return buffer;
-        } catch (Throwable e) {
-            ReferenceByteBufUtil.safeRelease(buffer);
-            log.error("rpcMsgEncoder encodeMsg error:", e);
-            throw e;
-        }
-
+        }, buffer);
     }
 
-    private ByteBuf tryCompress(ByteBuf input) {
-        CompositeByteBuf composite = input.alloc().compositeBuffer();
-        try {
-            EmbeddedChannel ch = compressChannel.get();
-            ch.releaseOutbound();
-            ch.writeOutbound(input);
-            ch.flushOutbound();
-            ByteBuf buf;
-            while ((buf = ch.readOutbound()) != null) {
-                composite.addComponent(true, buf);
+    private ByteBuf tryCompress(ByteBuf input, boolean isCompress) {
+        return ReferenceByteBufUtil.finallyRelease(() -> {
+            CompositeByteBuf composite = input.alloc().compositeBuffer();
+            composite.addComponent(true, input.alloc().buffer(1).writeBoolean(isCompress));
+            ByteBuf byteBuf = input.retain();
+            if (!isCompress) {
+                return composite.addComponent(true, byteBuf);
             }
-            return composite;
-        } catch (Throwable e) {
-            ReferenceByteBufUtil.safeRelease(input, composite);
-            log.error("rpcMsgEncoder tryCompress error:", e);
-            throw e;
-        }
+            return ReferenceByteBufUtil.exceptionRelease(() -> {
+                EmbeddedChannel ch = compressChannel.get();
+                ch.releaseOutbound();
+                ch.writeOutbound(byteBuf);
+                ch.flushOutbound();
+                ByteBuf buf;
+                while ((buf = ch.readOutbound()) != null) {
+                    composite.addComponent(true, buf);
+                }
+                return composite;
+            }, composite, byteBuf);
+        }, input);
     }
 }
