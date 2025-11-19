6a32b4585da065bb35fd6b227f52e20e2121157c
yaochuang <chuang.yao1081@seaboxdata.com>
2025-11-19 09:29:02 +0800

    删除注释

--- src/main/java/com/github/wohatel/interaction/base/RpcSessionFuture.java
+++ src/main/java/com/github/wohatel/interaction/base/RpcSessionFuture.java
@@ -14,11 +14,11 @@ import lombok.experimental.Accessors;
 @Accessors(chain = true)              // Enables method chaining for setters
 public class RpcSessionFuture extends RpcFuture {  // Extends RpcFuture for session-specific functionality
 
-    @Getter  // Automatically generates getter method for this field
-    @Setter  // Automatically generates setter method for this field
+    @Getter  
+    @Setter  
     private volatile RpcSessionProcess rpcSessionProcess;  // Session process handler, volatile for thread safety
 
-    @Getter  // Automatically generates getter method for this field
+    @Getter  
     @Setter
     private RpcSessionType rpcSessionType;
 
